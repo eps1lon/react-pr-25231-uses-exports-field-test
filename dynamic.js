1b60c61c20744551bc3c801359c9f81f10df8ea7
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-02-05 11:47:54 +0100

    Test legacy entrypoints

--- dynamic.js
+++ dynamic.js
@@ -1,9 +1,17 @@
 const entrypoints = [
   "use-sync-external-store",
   "use-sync-external-store/with-selector",
+  // legacy entrypoint
+  "use-sync-external-store/with-selector.js",
   "use-sync-external-store/shim",
+  // legacy entrypoint
+  "use-sync-external-store/shim/index.js",
   "use-sync-external-store/shim/index.native",
+  // legacy entrypoint
+  "use-sync-external-store/shim/index.native.js",
   "use-sync-external-store/shim/with-selector",
+  // legacy entrypoint
+  "use-sync-external-store/shim/with-selector.js",
   "use-sync-external-store/package.json",
 ];
 
@@ -15,9 +23,9 @@ async function main() {
         options.assert = { type: "json" };
       }
       const namespace = await import(entrypoint, options);
-      console.log(`PASS '${entrypoint}' exports: ${Object.keys(namespace)}`);
+      console.log(`PASS ${entrypoint} exports: ${Object.keys(namespace)}`);
     } catch (error) {
-      console.error(`FAIL ${entrypoint}:\n${error}`);
+      console.error(`FAIL ${entrypoint}: ${error.code}`);
     }
   }
 }
--- static.js
+++ static.js
@@ -2,9 +2,13 @@ import * as path from "path";
 import { fileURLToPath } from "url";
 import A from "use-sync-external-store";
 import B from "use-sync-external-store/with-selector";
+import legacy_B from "use-sync-external-store/with-selector.js";
 import C from "use-sync-external-store/shim";
+import legacy_C from "use-sync-external-store/shim/index.js";
 import D from "use-sync-external-store/shim/index.native";
+import legacy_D from "use-sync-external-store/shim/index.native.js";
 import E from "use-sync-external-store/shim/with-selector";
+import legacy_E from "use-sync-external-store/shim/with-selector.js";
 import F from "use-sync-external-store/package.json" assert { type: "json" };
 
 console.table([
